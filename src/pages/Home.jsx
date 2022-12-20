1b9b2a348d4f1832aa8e6ce121b440df0d3d4af5
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-12-20 21:05:43 +0530

    added authentication

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
-import {  useQuery } from "react-query";
-
-
+import { useQuery } from "react-query";
+import { useAuthContext } from "../hooks/useAuthContext";
 const Home = () => {
- 
+  const { user } = useAuthContext();
   const { isLoading, error, data } = useQuery(
     "workoutData",
     () =>
-      fetch("http://localhost:5000/api/workouts").then((res) => {
+      fetch("http://localhost:5000/api/workouts", {
+        headers: {
+          'Authorization': `Bearer ${user.token}`,
+        },
+      }).then((res) => {
         console.log("Res", res);
         return res.json();
       }),
-    {                                                             //! REACT QUERY FETCH
+    {
+      //! REACT QUERY FETCH
       cacheTime: 3000,
       staleTime: 0,
       refetchOnMount: "always",
@@ -21,12 +25,10 @@ const Home = () => {
     }
   );
 
-  
-
   //! REACT QUERY EXAMPLE
 
   if (isLoading) return "Loading...";
-  console.log("Data is", data);
+  // console.log("Data is", data);
 
   if (error) return "An error has occurred: " + error.message;
 
